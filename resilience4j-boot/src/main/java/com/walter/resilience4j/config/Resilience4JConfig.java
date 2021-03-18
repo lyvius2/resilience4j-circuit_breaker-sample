aee876d16a3b82bc972d02c4de84fec2040a579d
walter.hwang <lyvius2@naver.com>
2021-03-18 12:54:33 +0900

    lambda식으로 변경, TimeLimiter 설정 추가

--- resilience4j-boot/src/main/java/com/walter/resilience4j/config/Resilience4JConfig.java
+++ resilience4j-boot/src/main/java/com/walter/resilience4j/config/Resilience4JConfig.java
@@ -1,12 +1,14 @@
 package com.walter.resilience4j.config;
 
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.Duration;
 import java.util.function.Function;
 
 import static io.github.resilience4j.circuitbreaker.CircuitBreakerConfig.SlidingWindowType.COUNT_BASED;
@@ -23,12 +25,10 @@ public class Resilience4JConfig {
 				.build();
 
 		final CircuitBreakerFactory factory = new Resilience4JCircuitBreakerFactory();
-		factory.configureDefault(new Function<String, Resilience4JConfigBuilder.Resilience4JCircuitBreakerConfiguration>() {
-			@Override
-			public Resilience4JConfigBuilder.Resilience4JCircuitBreakerConfiguration apply(String s) {
-				return new Resilience4JConfigBuilder(s).circuitBreakerConfig(circuitBreakerConfig).build();
-			}
-		});
+		factory.configureDefault((Function<String, Resilience4JConfigBuilder.Resilience4JCircuitBreakerConfiguration>) s ->
+				new Resilience4JConfigBuilder(s)
+						.timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(2)).build())
+						.circuitBreakerConfig(circuitBreakerConfig).build());
 		return factory;
 	}
 }
